f66ab156a05b76337813cf7645ca1f9a53bcda78
juancarlosonate-tecnativa <juancarlos.onate@tecnativa.com>
2026-06-09 13:34:01 +0200

    [MIG] account_invoice_currency_by_partner: Migration to 18.0
    TT62359

--- account_invoice_currency_by_partner/__manifest__.py
+++ account_invoice_currency_by_partner/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
     "name": "Account Invoice Currency by Partner",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "Add currency to partner for invoice",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
